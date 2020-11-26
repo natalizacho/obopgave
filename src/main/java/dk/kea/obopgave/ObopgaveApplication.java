748c22892ab2ec67c3f0aeca0cc75cadf73df725
natalizacho <nata2716@stud.kea.dk>
2020-11-26 09:31:25 +0100

    Initial commit

---
+++ src/main/java/dk/kea/obopgave/ObopgaveApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.obopgave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObopgaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObopgaveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/obopgave/ObopgaveApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.obopgave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObopgaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
