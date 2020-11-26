047baf32e8450db5e62fd24f2e695411952ca18b
Pelle <pelle.park@gmail.com>
2020-11-26 14:14:14 +0100

    Hallo?

--- src/main/java/dk/kea/obopgave/controllers/HomeController.java
+++ src/main/java/dk/kea/obopgave/controllers/HomeController.java
@@ -1,13 +1,18 @@
 package dk.kea.obopgave.controllers;
 
+import dk.kea.obopgave.repository.DogRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    DogRepository DR = new DogRepository();
+
     @GetMapping("/")
-    public String index (){
+    public String index (Model model){
+        model.addAttribute("doglist", DR.readAll());
         return "index";
     }
 }
--- src/main/java/dk/kea/obopgave/utilities/DatabaseConnectionManager.java
+++ src/main/java/dk/kea/obopgave/utilities/DatabaseConnectionManager.java
@@ -1,16 +1,18 @@
 package dk.kea.obopgave.utilities;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DatabaseConnectionManager {
     private static DatabaseConnectionManager db = new DatabaseConnectionManager();
-    private static Connection conn;
+    private Connection conn;
 
     private DatabaseConnectionManager() {
-
         Properties pro = new Properties();
         try {
             FileInputStream fil = new FileInputStream("src/main/resources/application.properties");
@@ -20,9 +22,18 @@ public class DatabaseConnectionManager {
             String url = pro.getProperty("db.url");
             conn = DriverManager.getConnection(url, user, password);
         }
-        catch (Exception ignored){
+        catch(FileNotFoundException e){
+            System.out.println("File could not be found");
+            e.printStackTrace();
         }
-
+        catch(IOException e){
+            System.out.println("Property could not be loaded");
+            e.printStackTrace();
+        }
+        catch(SQLException e){
+            System.out.println("Message to the developer");
+            e.printStackTrace();
+        }
     }
 
     public Connection getConn(){
